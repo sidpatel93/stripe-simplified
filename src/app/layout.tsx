5e1c4d125a24d37e618582cbe53bb1d7f9749e0e
Sid Patel <sid.auburn@gmail.com>
2025-03-08 10:13:34 -0500

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ConvexClientProvider } from "@/components/providers/ConvexClientProvider";
+import NavBar from "@/components/NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-       <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider>
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+          <NavBar />
+          {children}
+        </ConvexClientProvider>
       </body>
     </html>
   );
